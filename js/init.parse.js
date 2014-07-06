bca9339be8f57f828fdc0bb2baee962213480c98
Collin Hartigan <lakers4life236@yahoo.com>
2014-07-06 00:22:35 -0700

    Added js

---
+++ js/init.parse.js
@@ -0,0 +1,8 @@
+$(function(){
+	$.parse.init({
+		app_id : "uPZ8a9bHKejO6SjSq2hxKBEoBfUk1QtkHbgZZqGs",
+		rest_key : "EuXJYhBHYDAkJkdVbzUkKuYsWiFziEANhBXuaajU"
+	});
+
+	Parse.initialize("uPZ8a9bHKejO6SjSq2hxKBEoBfUk1QtkHbgZZqGs", "2yCzLUFE0RDBpUc3KN3G7j5xRCRvMCqPUQBk25jB");
+});
